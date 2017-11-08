bb4c9bb05ed5ad80a208f7bd16f9c6fd41aa96bd
2maykealex <mayke.suporte@gmail.com>
2017-11-07 22:18:09 -0400

    Lógica do Login funcionando!

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
-from flask import Flask,render_template, request, url_for, redirect
-import flask_login
 
+from flask import Flask, render_template, request, url_for, redirect
+import flask_login
 
 app = Flask(__name__)
 app.secret_key = 'super secret string' #Change this!
+
 
 login_manager = flask_login.LoginManager()
 
 login_manager.init_app(app)
 
 users = {'teste@teste.com': {'password': '123'}}
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,7 +50,7 @@ def login():
             flask_login.login_user(user)
             return redirect(url_for('protected'))
 
-        return 'Bad login'
+        return 'Dados inválidos'
 
     return render_template('login.html') #vejam se é esse o nome do arquivo
 
@@ -32,8 +61,9 @@ def protected():
     return 'Logged in as: ' + flask_login.current_user.id
 
 @app.route('/')
+@app.route('/login')
 def index():
-    return '<a href="/login"> Login</a>' #vejam se é essa a rota do seu arquivo de login.html
+    return render_template('index.html')
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
