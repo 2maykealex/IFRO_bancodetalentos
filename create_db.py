efa916a0e9f9a64df066d6a17a1945f921c99d3c
Mayke <mayke.suporte>
2017-11-09 18:22:23 -0400

    Configurado o Banco app.db

--- create_db.py
+++ create_db.py
@@ -1,11 +1,10 @@
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-from app import db
+from models import db
 import os.path
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 
 db = SQLAlchemy(app)
@@ -9,13 +10,12 @@ class Pessoa(db.Model):
     id = db.Column('pessoa_id', db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
 
-
 class Telefone(db.Model):
-    id = db.column('telefone_id', db.Integer, primary_key=True)
+    id = db.Column('telefone_id', db.Integer, primary_key=True)
     telefone = db.Column(db.String(15))
 
 class Email(db.Model):
-    id = db.column('email_id', db.Integer, primary_key=True)
-    email = db.Column(db.String(60)
+    id = db.Column('email_id', db.Integer, primary_key=True)
+    email = db.Column(db.String(60))
 
 
