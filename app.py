6662198234f5e965fd1881d70d5af31f7a311725
Mayke <mayke.suporte@gmail.com>
2017-11-17 18:53:04 -0400

    Banco feito. Login OK

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect, flash
 import flask_login
-import models
 from flask_sqlalchemy import SQLAlchemy
+import models
+import config
 
 app = Flask(__name__)
 app.secret_key = 'super secret string' #Change this!
 
-
-db = SQLAlchemy(app) #db é uma instância de SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aluno:alunoifro@localhost/bancodetalentos'
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECURITY_REGISTERABLE'] = True
 
+db = SQLAlchemy(app) #db é uma instância de SQLAlchemy
 
 login_manager = flask_login.LoginManager()
 
@@ -25,7 +25,8 @@ class User(flask_login.UserMixin):
 
 @login_manager.user_loader
 def user_loader(email):
-    if email not in users:
+    email_user = models.Pessoa.query.filter_by(email=email).first()
+    if email_user is None:
         return
 
     user = User()
@@ -37,14 +38,14 @@ def user_loader(email):
 def request_loader(request):
     email = request.form.get('email')
     if email not in users:
-        return
+        return render_template('login.html')
 
     user = User()
     user.id = email
 
     # DO NOT ever store passwords in plaintext and always compare password
     # hashes using constant-time comparison!
-    user.is_authenticated = request.form['password'] == users[email]['password']
+    #user.is_authenticated = request.form['password'] == users[email]['password']
 
     return user
 
@@ -83,7 +84,10 @@ def saiba_mais():
 def login():
     if request.method == 'POST':
         email = request.form['email']
-        if request.form['password'] == users[email]['password']:
+
+        user_email = models.Pessoa.query.filter_by(email=email).first()
+
+        if request.form['password'] == user_email.password:
             user = User()
             user.id = email
             flask_login.login_user(user)
@@ -97,7 +101,7 @@ def login():
 @app.route('/protected')
 @flask_login.login_required
 def protected():
-    return render_template('home.html', flask_login.current_user.id) #'Logged in as: ' + flask_login.current_user.id
+    return render_template('home.html') #'Logged in as: ' + flask_login.current_user.id
 
 @app.route('/')
 @app.route('/login')
--- models.py
+++ models.py
@@ -1,15 +1,25 @@
 from app import db
 
 class Pessoa(db.Model):
-    id = db.Column('pessoa_id', db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(10))
+
+    def __init__(self, nome, email, password):
+        self.nome = nome
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<Pessoa %r>' % self.nome
 
 class Telefone(db.Model):
-    id = db.Column('telefone_id', db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     telefone = db.Column(db.String(15))
 
 class Email(db.Model):
-    id = db.Column('email_id', db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60))
 
 
