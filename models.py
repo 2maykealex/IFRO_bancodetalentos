5614fea5638afde1818b3772976be4ddaf86bcec
Mayke <mayke.suporte@gmail.com>
2017-11-20 17:28:26 -0400

    Relacionamentos

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy.orm import relationship
 
 class Pessoa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,6 +25,8 @@ class Aluno(Pessoa):
     # chaves estrangeiras
     pessoa_id = db.Column(db.String(15), db.ForeignKey('pessoa.id'))
 
+    alunos = relationship('Pessoa', backref= 'Aluno')
+
     def __init__(self, cpf, matricula, nome, email, password, tipo):        
         self.cpf = cpf
         self.matricula = matricula
@@ -44,12 +47,29 @@ class Telefone(db.Model):
     # chaves estrangeiras
     pessoa_id = db.Column(db.String(15), db.ForeignKey('pessoa.id'))
 
+    telefones = relationship('Pessoa', backref= 'Telefone')
+
+    def __init__(self, email):
+        self.telefone = telefone
+
+    def __repr__(self):
+        return '<Telefone %r>' % self.telefone
+
 class Email(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60))
 
     # chaves estrangeiras
     pessoa_id = db.Column(db.String(15), db.ForeignKey('pessoa.id'))
+
+    emails = relationship('Pessoa', backref= 'Email')
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<Email %r>' % self.email
+
 
 class Endereco(db.Model):
     id    = db.Column(db.Integer, primary_key=True)
@@ -62,8 +82,11 @@ class Endereco(db.Model):
     pessoa_id = db.Column(db.String(15), db.ForeignKey('pessoa.id'))
     cidade_id = db.Column(db.String(15), db.ForeignKey('cidade.id'))
 
+    enderecos = relationship('Pessoa', backref= 'Endereco')
+
+
     def __init__(self, nome, uf):
-        self.nome = endereco
+        self.endereco = endereco
         self.num = num
         self.complemento = complemento
         self.bairro = bairro        
@@ -78,6 +101,8 @@ class Cidade(db.Model):
     # chaves estrangeiras
     estado_id = db.Column(db.String(15), db.ForeignKey('estado.id'))
     
+    cidades = relationship('Endereco', backref= 'Cidade')
+
     def __init__(self, nome):
         self.nome = nome
 
@@ -88,6 +113,9 @@ class Estado(db.Model):
     id  = db.Column(db.Integer, primary_key=True)
     nome  = db.Column(db.String(50))
     uf  = db.Column(db.String(2))
+
+    estados = relationship('Cidade', backref= 'Estado')
+
 
     def __init__(self, nome, uf):
         self.nome = nome
