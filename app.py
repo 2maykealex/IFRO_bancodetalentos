0a20a3019952fce443b2af270277065c55736752
Mayke <mayke.suporte@gmail.com>
2017-11-19 21:29:10 -0400

    Revert "banco"
    This reverts commit 7262cd747257826dcc92341eb6119a69d0e44fad.

--- app.py
+++ app.py
@@ -72,15 +72,6 @@ def post_user():
  return redirect(url_for('index'))
 
 
-@app.route('/post_aluno', methods=['POST'])
-def post_aluno():
- aluno = models.User(request.form['username'], request.form['email'])
- models.db.session.add(aluno)
- models.db.session.commit()
- flash('Aluno registrado com sucesso!')
- return redirect(url_for('index'))
-
-
 @app.route('/cadAluno')
 def cadAluno():
     return render_template('cadAluno.html')
