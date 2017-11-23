7ea6bdbacf983a2628c4689b5325ccf4d141ed7f
Mayke <mayke.suporte@gmail.com>
2017-11-22 22:41:08 -0400

    Para importações

---
+++ imports.py
@@ -0,0 +1,9 @@
+import models
+from app import db
+from models import Aluno
+from models import Pessoa
+from models import Endereco
+from models import Cidade
+from models import Estado
+from models import Telefone
+from models import Email
