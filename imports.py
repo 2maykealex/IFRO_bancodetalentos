b85fd10bcfae1791cc418f4f96e9f12d3732d765
Mayke <mayke.suporte@gmail.com>
2017-11-22 22:42:23 -0400

    Importações

--- imports.py
+++ imports.py
@@ -7,3 +7,14 @@ from models import Cidade
 from models import Estado
 from models import Telefone
 from models import Email
+
+db.drop_all()
+
+db.create_all()
+
+aluno = Aluno('22222222222', '12345', 'mayke', 'mayke.suporte@gmail.com','123', 1)
+db.session.add(aluno)
+db.session.commit()
+
+Pessoa.
+
